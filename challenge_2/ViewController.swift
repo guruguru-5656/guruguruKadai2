c3dcc8956286e608be2d451708e91a8952430132
guruguru-5656 <100573472+morihonn-t@users.noreply.github.com>
2022-03-29 13:50:51 +0900

    完成した

--- challenge_2/ViewController.swift
+++ challenge_2/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet private weak var inputTextField_1: UITextField!
+    @IBOutlet private weak var inputTextField_2: UITextField!
+    @IBOutlet weak var calculatorSelector: UISegmentedControl!
+    @IBOutlet weak var resultLavel: UILabel!
+    private var calculator: (Double,Double) -> Double = { $0 + $1 }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func selectCalculator(_ sender: Any) {
+        switch calculatorSelector.selectedSegmentIndex {
+        case 0:
+            calculator = { $0 + $1 }
+        case 1:
+            calculator = { $0 - $1 }
+        case 2:
+            calculator = { $0 * $1 }
+        case 3:
+            calculator = { $0 / $1 }
+        default: return
+        }
+    }
+    @IBAction func calculate(_ sender: Any) {
+        guard let inputNumber_1 = Double(inputTextField_1.text ?? ""),
+              let inputNumber_2 = Double(inputTextField_2.text ?? "")
+        else{
+            resultLavel.text = "数字を入力してください"
+            return
+        }
+        if calculatorSelector.selectedSegmentIndex == 3 && inputNumber_2 == 0 {
+            resultLavel.text = "割る数には0以外を入力してください"
+            return
+        }
+        let result = calculator(inputNumber_1, inputNumber_2)
+        resultLavel.text = String(result)
+    }
 }
-
