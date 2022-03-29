201f5826467546d982afb48715822b317a2e5e51
guruguru-5656 <100573472+morihonn-t@users.noreply.github.com>
2022-03-29 10:52:55 +0900

    Initial Commit

---
+++ challenge_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  challenge_2
+//
+//  Created by 森本拓未 on 2022/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
