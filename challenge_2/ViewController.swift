7f3aa6aa63a842bd9070a6d3ef5391d49b85ae11
guruguru-5656 <100573472+morihonn-t@users.noreply.github.com>
2022-03-29 13:59:34 +0900

    swiftLintでコード修正

--- challenge_2/ViewController.swift
+++ challenge_2/ViewController.swift
@@ -8,15 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet private weak var inputTextField_1: UITextField!
-    @IBOutlet private weak var inputTextField_2: UITextField!
-    @IBOutlet weak var calculatorSelector: UISegmentedControl!
-    @IBOutlet weak var resultLavel: UILabel!
-    private var calculator: (Double,Double) -> Double = { $0 + $1 }
+    @IBOutlet private weak var inputTextFieldFirst: UITextField!
+    @IBOutlet private weak var inputTextFieldSecond: UITextField!
+    @IBOutlet private weak var calculatorSelector: UISegmentedControl!
+    @IBOutlet private weak var resultLavel: UILabel!
+    private var calculator: (Double, Double) -> Double = { $0 + $1 }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    @IBAction func selectCalculator(_ sender: Any) {
+    @IBAction private func selectCalculator(_ sender: Any) {
         switch calculatorSelector.selectedSegmentIndex {
         case 0:
             calculator = { $0 + $1 }
@@ -29,18 +29,18 @@ class ViewController: UIViewController {
         default: return
         }
     }
-    @IBAction func calculate(_ sender: Any) {
-        guard let inputNumber_1 = Double(inputTextField_1.text ?? ""),
-              let inputNumber_2 = Double(inputTextField_2.text ?? "")
-        else{
+    @IBAction private func calculate(_ sender: Any) {
+        guard let inputNumberFirst = Double(inputTextFieldFirst.text ?? ""),
+              let inputNumberSecond = Double(inputTextFieldSecond.text ?? "")
+        else {
             resultLavel.text = "数字を入力してください"
             return
         }
-        if calculatorSelector.selectedSegmentIndex == 3 && inputNumber_2 == 0 {
+        if calculatorSelector.selectedSegmentIndex == 3 && inputNumberSecond == 0 {
             resultLavel.text = "割る数には0以外を入力してください"
             return
         }
-        let result = calculator(inputNumber_1, inputNumber_2)
+        let result = calculator(inputNumberFirst, inputNumberSecond)
         resultLavel.text = String(result)
     }
 }
